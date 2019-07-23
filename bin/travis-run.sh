d2119b405af2feed0c456f32aa6f4debe3e56bcd
Knud Möller <knud@datalysator.com>
2019-07-23 13:58:53 +0200

    fix cover-package setting in travis-run.sh

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -8,7 +8,7 @@ nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
           --with-coverage \
-          --cover-package=ckanext.validationapi \
+          --cover-package=ckanext.fisbroker \
           --cover-inclusive \
           --cover-erase \
           --cover-tests
