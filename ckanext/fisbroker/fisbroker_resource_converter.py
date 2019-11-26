a29259f042d29b8885ab61f14a37cfab2231c867
Knud Möller <knud@datalysator.com>
2019-11-26 15:39:55 +0100

    add failing test for GetCapabilitites resources

--- ckanext/fisbroker/fisbroker_resource_converter.py
+++ ckanext/fisbroker/fisbroker_resource_converter.py
@@ -9,4 +9,30 @@ class FISBrokerResourceConverter:
 
     def convert_resource(self, resource):
         """Assign meaningful metadata to FIS-Broker resource objects from a CKAN package_dict, based on their URLs."""
-        return "bad bad boy"
+        if "/feed/" in resource['url']:
+            resource['name'] = "Atom Feed"
+            resource['description'] = "Atom Feed"
+            resource['format'] = "Atom"
+            resource['main'] = True
+        elif "/wfs/" in resource['url']:
+            resource['name'] = "WFS Service"
+            resource['description'] = "WFS Service"
+            resource['format'] = "WFS"
+            resource['url'] += "?service=wfs&request=GetCapabilities"
+            resource['main'] = True
+        elif "/wms/" in resource['url']:
+            resource['name'] = "WMS Service"
+            resource['description'] = "WMS Service"
+            resource['format'] = "WMS"
+            resource['url'] += "?service=wms&request=GetCapabilities"
+            resource['main'] = True
+        elif resource['url'].startswith('https://fbinter.stadt-berlin.de/fb?loginkey='):
+            resource['name'] = "Serviceseite im FIS-Broker"
+            resource['format'] = "HTML"
+            resource['description'] = "Serviceseite im FIS-Broker"
+        elif resource['description']:
+            resource['name'] = resource['description']
+            resource['main'] = False
+        else:
+            resource = None
+        return resource
--- ckanext/fisbroker/tests/test_resource_converter.py
+++ ckanext/fisbroker/tests/test_resource_converter.py
@@ -1,8 +1,13 @@
 # coding: utf-8
 
-import ckanext.fisbroker.fisbroker_resource_converter
+from ckanext.fisbroker.fisbroker_resource_converter import FISBrokerResourceConverter
 
 class TestResourceConverter(object):
 
     def test_convert_getcapabilities(self):
-        pass
+        resource = { 
+            'url': 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015?request=getcapabilities&service=wfs&version=2.0.0'
+        }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+        assert converted_resource['url'] == resource['url']
