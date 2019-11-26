f42fedff8f866337ded9205e5bc69ee1082874a9
Knud Möller <knud@datalysator.com>
2019-11-26 15:23:05 +0100

    initialize work on fisbroker resource converter with tests

---
+++ ckanext/fisbroker/fisbroker_resource_converter.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class FISBrokerResourceConverter:
+    """A class to assign meaningful metadata to FIS-Broker resource objects from a CKAN package_dict, based on their URLs."""
+
+    def convert_resource(self, resource):
+        """Assign meaningful metadata to FIS-Broker resource objects from a CKAN package_dict, based on their URLs."""
+        return "bad bad boy"
---
+++ ckanext/fisbroker/tests/test_resource_converter.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+import ckanext.fisbroker.fisbroker_resource_converter
+
+class TestResourceConverter(object):
+
+    def test_convert_getcapabilities(self):
+        pass
