22bac627af9cd52ee98ea771c7feb3ecb59c707a
Knud Möller <knud@datalysator.com>
2019-11-27 17:12:06 +0100

    add resource building, conversion and URL normalization with tests

--- ckanext/fisbroker/fisbroker_resource_converter.py
+++ ckanext/fisbroker/fisbroker_resource_converter.py
@@ -1,36 +1,67 @@
 # coding: utf-8
 
 import logging
+from urlparse import urlparse, urlunparse, parse_qs
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 class FISBrokerResourceConverter:
     """A class to assign meaningful metadata to FIS-Broker resource objects from a CKAN package_dict, based on their URLs."""
 
+    @staticmethod
+    def getcapabilities_suffix():
+        return "request=GetCapabilities&version=2.0.0"
+
+    def normalize_url(self, url):
+        """Normalize URL by sorting query parameters and lowercasing the values
+           (because in parameter values are not case sensitive in WMS/WFS)."""
+
+        parsed = urlparse(url)
+        query = parse_qs(parsed.query)
+
+        normalized_query = []
+        for parameter in sorted(query):
+            normalized_query.append("{}={}".format(parameter, query[parameter][0].lower()))
+
+        return urlunparse(parsed._replace(query="&".join(normalized_query)))
+
+    def build_service_resource(self, resource):
+        """Build either a WMS or WFS service resource from service URL."""
+        
+        if "/wfs/" in resource['url']:
+            service_type = "WFS"
+        elif "/wms/" in resource['url']:
+            service_type = "WMS"
+        else:
+            return resource
+
+        parsed = urlparse(resource['url'])
+        query = parse_qs(parsed.query)
+        if not query:
+            resource['url'] += "?service={}&{}".format(service_type.lower(), FISBrokerResourceConverter.getcapabilities_suffix())
+
+        resource['name'] = "{} Service".format(service_type)
+        resource['description'] = "{} Service".format(service_type)
+        resource['format'] = service_type
+
+        return resource
+
     def convert_resource(self, resource):
         """Assign meaningful metadata to FIS-Broker resource objects from a CKAN package_dict, based on their URLs."""
+
         if "/feed/" in resource['url']:
             resource['name'] = "Atom Feed"
             resource['description'] = "Atom Feed"
             resource['format'] = "Atom"
             resource['main'] = True
-        elif "/wfs/" in resource['url']:
-            resource['name'] = "WFS Service"
-            resource['description'] = "WFS Service"
-            resource['format'] = "WFS"
-            resource['url'] += "?service=wfs&request=GetCapabilities"
-            resource['main'] = True
-        elif "/wms/" in resource['url']:
-            resource['name'] = "WMS Service"
-            resource['description'] = "WMS Service"
-            resource['format'] = "WMS"
-            resource['url'] += "?service=wms&request=GetCapabilities"
+        elif "/wfs/" in resource['url'] or "/wms/" in resource['url']:
+            resource = self.build_service_resource(resource)
             resource['main'] = True
         elif resource['url'].startswith('https://fbinter.stadt-berlin.de/fb?loginkey='):
             resource['name'] = "Serviceseite im FIS-Broker"
             resource['format'] = "HTML"
             resource['description'] = "Serviceseite im FIS-Broker"
-        elif resource['description']:
+        elif 'description' in resource:
             resource['name'] = resource['description']
             resource['main'] = False
         else:
--- ckanext/fisbroker/tests/test_resource_converter.py
+++ ckanext/fisbroker/tests/test_resource_converter.py
@@ -1,13 +1,142 @@
 # coding: utf-8
 
+import logging
 from ckanext.fisbroker.fisbroker_resource_converter import FISBrokerResourceConverter
+
+log = logging.getLogger(__name__)
 
 class TestResourceConverter(object):
 
+    def _assert_equal(self, actual, expected):
+        log.debug("expected: %s", expected)
+        log.debug("actual:   %s", actual)
+        assert expected == actual
+
     def test_convert_getcapabilities(self):
-        resource = { 
-            'url': 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015?request=getcapabilities&service=wfs&version=2.0.0'
+        """An incoming FIS-Broker service URL with URL parameters containing 'request=GetCapabilities'
+           should be returned unchanged."""
+
+        url = 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015?request=getcapabilities&service=wfs&version=2.0.0'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+        
+        self._assert_equal(converted_resource['url'], url)
+
+    def test_plain_service_wfs_url(self):
+        """An incoming FIS-Broker WFS service URL without URL parameters containing 'request=GetCapabilities'
+           should be concatenated with said URL parameters."""
+
+        url = 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+        
+        self._assert_equal('{}{}'.format(url, "?service=wfs&{}".format(FISBrokerResourceConverter.getcapabilities_suffix())), converted_resource['url'])
+
+    def test_plain_service_wms_url(self):
+        """An incoming FIS-Broker WMS service URL without URL parameters containing 'request=GetCapabilities'
+           should be concatenated with said URL parameters."""
+
+        url = 'https://fbinter.stadt-berlin.de/fb/wms/senstadt/wmsk_02_14_04gwtemp_60m'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+        
+        self._assert_equal('{}{}'.format(url, "?service=wms&{}".format(FISBrokerResourceConverter.getcapabilities_suffix())), converted_resource['url'])
+
+    def test_atom_feed_url(self):
+        url = 'https://fbinter.stadt-berlin.de/fb/feed/senstadt/a_SU_LOR'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+
+        self._assert_equal(converted_resource['url'], url)
+        self._assert_equal(converted_resource['name'], "Atom Feed")
+        self._assert_equal(converted_resource['description'], "Atom Feed")
+        self._assert_equal(converted_resource['format'], "Atom")
+
+    def test_service_page_url(self):
+        url = 'https://fbinter.stadt-berlin.de/fb?loginkey=showMap&mapId=k01_11_07naehr2015@senstadt'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+
+        self._assert_equal(converted_resource['url'], url)
+        self._assert_equal(converted_resource['name'], "Serviceseite im FIS-Broker")
+        self._assert_equal(converted_resource['description'], "Serviceseite im FIS-Broker")
+        self._assert_equal(converted_resource['format'], "HTML")
+
+
+    def test_arbitrary_url_with_description(self):
+        url = 'https://fbinter.stadt-berlin.de/fb_daten/beschreibung/umweltatlas/datenformatbeschreibung/Datenformatbeschreibung_kriterien_zur_bewertung_der_bodenfunktionen2015.pdf'
+        description = 'Technische Beschreibung'
+        res_format = 'PDF'
+        resource = {
+            'url': url ,
+            'name': description ,
+            'description': description ,
+            'format': res_format
         }
         converter = FISBrokerResourceConverter()
         converted_resource = converter.convert_resource(resource)
-        assert converted_resource['url'] == resource['url']
+
+        self._assert_equal(converted_resource['name'], description)
+        self._assert_equal(converted_resource['description'], description)
+        self._assert_equal(converted_resource['format'], res_format)
+        self._assert_equal(converted_resource['url'], url)
+
+    def test_arbitrary_url_without_description(self):
+        url = 'https://fbinter.stadt-berlin.de/fb_daten/beschreibung/umweltatlas/datenformatbeschreibung/Datenformatbeschreibung_kriterien_zur_bewertung_der_bodenfunktionen2015.pdf'
+        res_format = 'PDF'
+        resource = {
+            'url': url ,
+            'name': 'Technische Beschreibung' ,
+            'format': res_format
+        }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.convert_resource(resource)
+
+        self._assert_equal(converted_resource, None)
+
+    def test_build_wms_resource(self):
+        """Build WMS service resource from a WMS service URL."""
+
+        url = 'https://fbinter.stadt-berlin.de/fb/wms/senstadt/wmsk_02_14_04gwtemp_60m'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        service_resource = converter.build_service_resource(resource)
+
+        self._assert_equal(service_resource['url'], "{}?service=wms&{}".format(url, FISBrokerResourceConverter.getcapabilities_suffix()))
+        self._assert_equal(service_resource['name'], "WMS Service")
+        self._assert_equal(service_resource['description'], "WMS Service")
+        self._assert_equal(service_resource['format'], "WMS")
+
+    def test_build_wfs_resource(self):
+        """Build WFS service resource from a WFS service URL."""
+
+        url = 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        service_resource = converter.build_service_resource(resource)
+
+        self._assert_equal(service_resource['url'], "{}?service=wfs&{}".format(url, FISBrokerResourceConverter.getcapabilities_suffix()))
+        self._assert_equal(service_resource['name'], "WFS Service")
+        self._assert_equal(service_resource['description'], "WFS Service")
+        self._assert_equal(service_resource['format'], "WFS")
+
+    def test_atom_feed_resource_returned_unchanged(self):
+        url = 'https://fbinter.stadt-berlin.de/fb/feed/senstadt/a_SU_LOR'
+        resource = { 'url': url }
+        converter = FISBrokerResourceConverter()
+        converted_resource = converter.build_service_resource(resource)
+
+        self._assert_equal(converted_resource['url'], url)
+        self._assert_equal(len(converted_resource), 1)
+
+    def test_normalize_url(self):
+        url1 = 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015?request=getcapabilities&service=wfs&version=2.0.0'
+        url2 = 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_boden_wfs1_2015?service=wfs&version=2.0.0&request=GetCapabilities'
+
+        converter = FISBrokerResourceConverter()
+        self._assert_equal(converter.normalize_url(url1), converter.normalize_url(url2))
