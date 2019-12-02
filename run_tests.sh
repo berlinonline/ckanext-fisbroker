d15b70915a7641c1c03d65c919121965fd21b4ab
Knud Möller <knud@datalysator.com>
2019-12-02 10:43:06 +0100

    set coverage output directory explicitly and .gitignore it

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-nosetests -s --verbosity=2 --ckan --with-pylons=test.ini --with-coverage --cover-package=ckanext.fisbroker --cover-erase --cover-html ckanext/fisbroker/tests/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+nosetests -s --verbosity=2 --ckan --with-pylons=test.ini --with-coverage --cover-package=ckanext.fisbroker --cover-erase --cover-html ckanext/fisbroker/tests/ --cover-html-dir=$DIR/cover
