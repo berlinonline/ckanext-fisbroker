ce09422febb90425aa2bc8b062fbcedd9d06ebf0
Knud Möller <knud@datalysator.com>
2020-03-27 18:22:31 +0100

    format nosetests command

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-nosetests -s --verbosity=2 --ckan --with-pylons=test.ini --with-coverage --cover-package=ckanext.fisbroker --cover-erase --cover-html ckanext/fisbroker/tests/$1 --cover-html-dir=$DIR/cover
+nosetests \
+          --ckan \
+          --with-pylons=test.ini \
+          --with-coverage \
+          --cover-package=ckanext.fisbroker \
+          --cover-erase \
+          --cover-html \
+          --cover-html-dir=$DIR/cover \
+          ckanext/fisbroker/tests/$1
+          # -s \
+          # --verbosity=2 \
